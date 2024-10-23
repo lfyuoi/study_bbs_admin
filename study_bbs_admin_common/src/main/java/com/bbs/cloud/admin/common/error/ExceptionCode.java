9931094c0bd1d46e54a0945524e9fb3dbfb16371
lfyuoi <1134091874@qq.com>
2024-10-23 10:33:55 +0800

    Revert "项目基本框架"
    This reverts commit ba4daeb7e5020f2eda586216277f1caf110fb640.

--- study_bbs_admin_common/src/main/java/com/bbs/cloud/admin/common/error/ExceptionCode.java
+++
@@ -1,9 +0,0 @@
-package com.bbs.cloud.admin.common.error;
-
-public interface ExceptionCode {
-
-    public Integer getCode();
-
-    public String getMessage();
-
-}
--- study_bbs_admin_component/study_bbs_admin_test/src/main/java/com/bbs/cloud/admin/test/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bbs.cloud.admin.test.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-
-    String queryTest();
-}
